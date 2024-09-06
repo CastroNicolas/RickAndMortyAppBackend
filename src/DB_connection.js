b7aa424eeb55da0a20824a2a158e36fc6edd51e7
CastroNicolas <maximonicolascastro@gmail.com>
2024-09-07 01:06:18 +0200

    test

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 const favoriteModel = require("./models/Favorite");
 const userModel = require("./models/User");
 
 const sequelize = new Sequelize(
-  ` postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-  { logging: false, native: false }
+  ` postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  { logging: false, native: false, dialect: "postgres" }
 );
 favoriteModel(sequelize);
 userModel(sequelize);
