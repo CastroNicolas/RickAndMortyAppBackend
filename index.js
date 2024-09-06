3306eebf41a6f2013df8aa6a347cd0a1d60275d8
CastroNicolas <maximonicolascastro@gmail.com>
2024-09-06 22:37:04 +0200

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./src/App");
 const { conn } = require("./src/DB_connection");
 require("dotenv").config();
 
-const DB_PORT = process.env.DB_PORT;
+const DB_PORT = 5000;
 
 server.listen(DB_PORT, async () => {
   await conn.sync({ force: true });
